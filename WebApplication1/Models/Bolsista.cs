f87b924ee213db46ce8bbcaa3e74b67f9973c6ee
RAFAEL SABINO DE SOUSA <20252160003@ifba.edu.br>
2026-03-24 18:42:51 +0000

    Revert "Merge branch '2-teste_semana_1' into 'main'"
    This reverts merge request !1

--- WebApplication1/Models/Bolsista.cs
+++ WebApplication1/Models/Bolsista.cs
@@ -17,29 +17,5 @@ namespace WebApplication1.Models
         // TODO: Criar a propriedade para a Data de Nascimento
 
         // TODO: Criar a propriedade para o Sexo
-
-        // TODO: Criar método com o resumo das informações contendo nome e matrícula
-
-        //TODO: Criar método que calcúla a idade do bolsista
-
-        public string CPF { get; set; }
-
-        public string Matricula { get; set; }
-
-        public DateTime DataNascimento { get; set; }
-
-        public string Sexo { get; set; }
-
-        public string ObterResumo()
-        {
-            return $"Bolsista: {Nome} (Matrícula: {Matricula})";
-        }
-
-        public int CalcularIdade()
-        {
-            int idade = DateTime.Now.Year - DataNascimento.Year;
-            return idade;
-        }
-
     }
 }
