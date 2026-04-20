f63d67fe50ddbf7c8f0da5d616242364e1b82bcc
Uriel-CdS <uriel2405correia@gmail.com>
2026-04-20 10:55:29 -0300

    ClasseProjeto

---
+++ WebApplication1/Models/Projeto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Projeto
+    {
+        public string Nome { get; set;  }
+        public float VerbaAprovada { get; set; }
+        public float BolsaIndividual { get; set; }
+        public Coordenador CoordenadorResponsavel { get; set; }
+        public List<Bolsista> BolsistasVinculados { get; set; }
+
+        public Projeto()
+        {
+            BolsistasVinculados = new List<Bolsista>();
+        }
+    }
+}
